0073f7bf48b0402ab910da9008d1a57804308530
dmg <dahimindgame@gmail.com>
2021-03-25 01:12:39 +0300

    cc

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import 'bootstrap';
+import 'popper.js';
+import jQuery from 'jquery';
+import './assets/app.scss';
+window.$ = window.jQuery = jQuery;
 
+//Vue.component('Navbar', require('./components/Navbar.vue').default);
 Vue.config.productionTip = false;
 
 new Vue({
