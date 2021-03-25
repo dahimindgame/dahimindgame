d76d9ec5f7b6b8098cbc46c2d908d4a86bdeb9ff
dmg <dahimindgame@gmail.com>
2021-03-26 01:01:53 +0300

    smb

---
+++ src/components/SocialMediaButtons.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="">
+      <a class="navbar-brand" href="https://www.youtube.com" target="_blank"><font-awesome-icon :icon="['fab', 'youtube']" class="icon alt"/></a>
+      <a class="navbar-brand" href="https://www.instagram.com" target="_blank"><font-awesome-icon :icon="['fab', 'instagram']" class="icon alt"/></a>
+      <a class="navbar-brand" href="https://www.facebook.com" target="_blank"><font-awesome-icon :icon="['fab', 'facebook']" class="icon alt"/></a>
+      <a class="navbar-brand" href="https://www.twitter.com" target="_blank"><font-awesome-icon :icon="['fab', 'twitter']" class="icon alt"/></a>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: "SocialMediaButtons",
+    props: {
+      msg: String,
+    },
+  };
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTwitter, faFacebook, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faTwitter, faFacebook, faYoutube, faInstagram);
+
+
 import 'bootstrap';
 import 'popper.js';
 import jQuery from 'jquery';
 import './assets/app.scss';
 window.$ = window.jQuery = jQuery;
 
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 //Vue.component('Navbar', require('./components/Navbar.vue').default);
 Vue.config.productionTip = false;
 
